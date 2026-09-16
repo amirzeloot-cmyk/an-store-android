0b02410b6ce29d634e6df6a75e5f96e71ac67a18
amirzeloot-cmyk <ebrahimzadeh13128584@gmail.com>
2026-09-15 19:07:44 -0700

    Capacitor Android wrapper + APK build workflow

---
+++ www/sw.js
@@ -0,0 +1,17 @@
+﻿const CACHE = "an-store-v4";
+const ASSETS = ["./", "./index.html", "./about.html", "./admin.html", "./manifest.json", "./assets/icon-192.png", "./assets/icon-512.png", "./assets/icon-maskable-512.png", "./assets/uc-oneone.png", "./assets/hero-pubg.jpg"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(c=>c.put(e.request, copy));
+    return res;
+  }).catch(()=> caches.match("./index.html"))));
+});
